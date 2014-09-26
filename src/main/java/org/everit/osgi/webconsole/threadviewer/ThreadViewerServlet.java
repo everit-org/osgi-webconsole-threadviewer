87dd03c09d59ddd4bb02388f1a4443332ad1266c
Bence Eros <bence.eros@everit.biz>
2014-09-26 12:19:33 +0200

    js/css file resource loading - proof-of-concept

--- src/main/java/org/everit/osgi/webconsole/threadviewer/ThreadViewerServlet.java
+++ src/main/java/org/everit/osgi/webconsole/threadviewer/ThreadViewerServlet.java
@@ -1,6 +1,8 @@
 package org.everit.osgi.webconsole.threadviewer;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +13,24 @@ import org.apache.felix.webconsole.AbstractWebConsolePlugin;
 public class ThreadViewerServlet extends AbstractWebConsolePlugin {
     private static final long serialVersionUID = -433422756955420082L;
 
+    // @Override
+    // protected String[] getCssReferences() {
+    // return new String[] { "/thread-viewer/hello/world.css" };
+    // }
+
     @Override
     public String getLabel() {
-        return "EOSGi Thread Viewer";
+        return "threads";
+    }
+
+    public URL getResource(final String path) {
+        System.out.println(path);
+        if (path.indexOf("hello/world.js") != -1) {
+            URL rval = getResourceProvider().getClass().getResource("/res/hello/world.js");
+            System.out.println("returning " + rval);
+            return rval;
+        }
+        return null;
     }
 
     @Override
@@ -25,7 +42,9 @@ public class ThreadViewerServlet extends AbstractWebConsolePlugin {
     protected void renderContent(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException,
             IOException {
-
+        Map<Thread, StackTraceElement[]> stackTraces = Thread.getAllStackTraces();
+        response.getWriter().println("hello world");
+        response.getWriter().println("<script src=\"/system/console/threads/hello/world.js\"></script>");
     }
 
 }
